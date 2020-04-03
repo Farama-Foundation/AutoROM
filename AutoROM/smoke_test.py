a5ce30079a9660abd5672703ec32971e13e2e732
marioj <mario.jayakumar@gmail.com>
2020-04-03 16:30:32 -0400

    fixed ROM names, added freeze list to smoke test

--- AutoROM/smoke_test.py
+++ AutoROM/smoke_test.py
@@ -5,7 +5,10 @@ install_dir = ale_py.__file__
 install_dir = install_dir[:-11] + "ROM/"
 
 game_list = []
+freeze_list = ["backgammon"]
 
+for f in freeze_list:
+    print("Ignoring ",f)
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 new_link_file = "link_map.txt"
@@ -13,7 +16,8 @@ link_file_ref = open(os.path.join(__location__, new_link_file), "r")
 for l in link_file_ref:
     payload = l.split("^^^")
     game_name = install_dir + payload[0] + "/"+payload[0] + ".bin"
-    game_list.append(game_name)
+    if not payload[0] in freeze_list:
+        game_list.append(game_name)
 
 ale = ale_py.ALEInterface()
 for g in game_list:
