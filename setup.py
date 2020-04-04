b3375c69f77d4094d11ce2602e86434f74821cb1
marioj <mario.jayakumar@gmail.com>
2020-04-03 22:03:10 -0400

    new pypi version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AutoROM",
-    version="0.1.9",
+    version="0.1.10",
     author="PettingZoo Team",
     author_email="justinkterry@gmail.com",
     description="Automated installation of Atari ROMs for Gym/ALE-Py",
