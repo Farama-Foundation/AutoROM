b1f396089d0377a753511088ed4e236e71953773
marioj <mario.jayakumar@gmail.com>
2020-05-23 17:44:15 -0400

    multi-agent ale compatability

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AutoROM",
-    version="0.1.12",
+    version="0.1.13",
     author="PettingZoo Team",
     author_email="justinkterry@gmail.com",
     description="Automated installation of Atari ROMs for Gym/ALE-Py",
@@ -18,7 +18,8 @@ setuptools.setup(
         'ale_py',
         'requests',
         'pyunpack',
-        'tqdm'
+        'tqdm',
+        'multi-agent-ale-py'
     ],
     python_requires='>=3.5',
     data_files=[("", ["LICENSE.txt"])],
