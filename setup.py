9b68f4060f2d60e606078b535ad4187c1f7ba60b
Justin Terry <justinkterry@gmail.com>
2020-12-15 11:51:44 -0500

    update supported python versions

--- setup.py
+++ setup.py
@@ -20,12 +20,12 @@ setuptools.setup(
         'tqdm',
         'patool'
     ],
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
