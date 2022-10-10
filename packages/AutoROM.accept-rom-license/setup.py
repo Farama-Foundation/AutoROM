5b0825d9134e83a57ab661b6c7c192907415db52
Jet <taijunjet@hotmail.com>
2022-10-10 21:38:00 +0100

    change some thing

--- packages/AutoROM.accept-rom-license/setup.py
+++ packages/AutoROM.accept-rom-license/setup.py
@@ -3,11 +3,13 @@ import pathlib
 
 from setuptools.command.install import install
 
+
 class InstallCommand(install):
     def run(self):
         super().run()
 
         from AutoROM import main as AutoROM
+
         download_dir = pathlib.Path(self.install_lib) / "AutoROM" / "roms"
         download_dir.mkdir(exist_ok=False, parents=True)
         AutoROM(True, download_dir, False)
