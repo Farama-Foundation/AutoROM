4a14422f8b1d2323d49fb0684c2df5bf653b3ab0
jjshoots <taijunjet@hotmail.com>
2023-02-16 22:46:14 +0000

    move alternate download elsewhere

---
+++ resource/alternate_download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/jjshoots/AutoROM/master/resource/Roms.tar.gz.b64.enc
+openssl aes-256-cbc -a -salt -pass pass:$DECRYPTION_KEY -in Roms.tar.gz.b64.enc -out Roms.tar.gz.b64 -d
+base64 Roms.tar.gz.b64 --decode &> Roms.tar.gz
