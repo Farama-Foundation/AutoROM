a9dae1f48e08006c40b14a5f7de09f3b3ee0d1bd
jjshoots <taijunjet@hotmail.com>
2023-02-16 22:47:46 +0000

    do i t more times

--- resource/alternate_download.sh
+++ resource/alternate_download.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 echo $DUMMY_KEY
+echo $DUMMY_KEY
+echo $DUMMY_KEY
+echo $DUMMY_KEY
+echo $DUMMY_KEY
+echo $DUMMY_KEY
+exit
 wget https://raw.githubusercontent.com/jjshoots/AutoROM/master/resource/Roms.tar.gz.b64.enc
 openssl aes-256-cbc -a -salt -pass pass:$DECRYPTION_KEY -in Roms.tar.gz.b64.enc -out Roms.tar.gz.b64 -d
 base64 Roms.tar.gz.b64 --decode &> Roms.tar.gz
